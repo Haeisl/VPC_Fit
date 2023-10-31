b4c2a873be3e95bf779fdbf6c3a2e629358d07d7
uo224 <alisa-ebert@web.de>
2023-10-31 12:06:49 +0100

    Update ModelFitter.py

--- src/ModelFitter.py
+++ src/ModelFitter.py
@@ -1,31 +1,26 @@
 import numpy as np
 
 class ModelFitter():
+    """This is a class to handle the model fitter
+
+    :param equation: the equation of the function to be fitted
+    :type equation: str
+    :param parameter: fitted parameter of the function
+    :type parameter: any, optional
     """
-    A class to handle the model fitter
-
-    ...
-
-    Attributes
-    ----------
-
-    equation: str
-        equation of the function to be fitted
-    parameter: any
-        fitted parameters of the function
-
-    
-    Methods
-    -------
-    fit():
-        returns the fitted parameters of the function equation based on the given data
-
-    """
-
 
     def __init__(self, equation):
+        """Constructor method
+        """
         self.equation = equation
         self.parameter = None
 
     def fit(self, data, equation):
+        """returns the fitted parameters of the given equation based on the input data 
+
+        :param data: _description_
+        :type data: _type_
+        :param equation: _description_
+        :type equation: _type_
+        """
         pass
