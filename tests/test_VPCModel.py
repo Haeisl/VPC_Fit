c98983bccaddf0a2757b419171e243af93c64194
David Hasse <david_h_hasse@yahoo.de>
2024-02-27 19:17:15 +0100

    oh

--- tests/test_VPCModel.py
+++ tests/test_VPCModel.py
@@ -10,6 +10,8 @@ def quad_model_clean() -> VPCModel:
     return VPCModel("f(t) = a*t**2 + b*t + c", ["t"])
 
 
-def test_model_string_getter(quad_model_unclean: VPCModel, quad_model_clean: VPCModel) -> None:
+def test_model_string_getter_unclean(quad_model_unclean: VPCModel) -> None:
     assert quad_model_unclean.model_string == "f(t) = a*t**2 + b*t + c"
+
+def test_model_string_getter_clean(quad_model_clean: VPCModel) -> None:
     assert quad_model_clean.model_string == "f(t) = a*t**2 + b*t + c"
