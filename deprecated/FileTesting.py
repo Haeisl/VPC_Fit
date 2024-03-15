b2ba633f206fabf4180143171efdbf7403072cb1
David Hasse <david_h_hasse@yahoo.de>
2024-03-15 01:11:00 +0100

    Revert "Revert "Revert "Revert "Revert "Delete deprecated directory"""""
    This reverts commit 4ddd200f185338327c8e9ab02b69046f22c07ac8.

---
+++ deprecated/FileTesting.py
@@ -0,0 +1,131 @@
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+import os
+
+def read_file(file_path: str) -> pd.DataFrame:
+    if not Path(file_path).is_file():
+        raise FileNotFoundError('Invalid Path; no file at destination')
+
+    suffix = Path(file_path).suffix.split('.')[1]
+    if suffix == '':
+        raise ValueError('No Fileextension found at path')
+
+    if suffix.upper() == 'XLSX':
+        data_frame = pd.read_excel(file_path)
+    elif suffix.upper() == 'CSV':
+        data_frame = pd.read_csv(file_path)
+    else:
+        raise TypeError('Unknown File extension')
+
+    return data_frame
+
+
+def dataframe_tolist(data_frame: pd.DataFrame, include_first_row: bool) -> list[list]:
+    if data_frame is None:
+        raise ValueError('No data could be read')
+
+    column_names = data_frame.columns.tolist()
+
+    if include_first_row:
+        data_list = [[name, *data_frame[name].tolist()] for name in column_names]
+    else:
+        data_list = [data_frame[name].tolist() for name in column_names]
+
+    return data_list
+
+
+def create_dataframe_from_for(
+    fitted_model: str = 'f(t) = ...',
+    model: str = 'f(t) = ...',
+    user_input_model: str = 'f(t) = ...',
+    parameter: str = '...',
+    user_input_parameter: str = '...',
+    consts: str = '...',
+    user_input_consts: str = '...',
+    user_input_path: str = 'path/to/data',
+    format: str = 'Excel'
+) -> pd.DataFrame:
+
+    if format == 'Excel':
+        data = pd.DataFrame(
+            index=range(1, 13),
+            columns=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+        )
+
+        data.at[1,'A'] = 'Fitted Model'
+        data.at[1,'C'] = fitted_model
+        data.at[4,'A'] = 'Interpreted Data'
+        data.at[4,'G'] = 'Raw Data'
+        data.at[6,'A'] = 'Model'
+        data.at[6,'C'] = model
+        data.at[6,'G'] = 'Entered Model'
+        data.at[6,'I'] = user_input_model
+        data.at[8,'A'] = 'Parameter'
+        data.at[8,'C'] = parameter
+        data.at[8,'G'] = 'Entered Parameter'
+        data.at[8,'I'] = user_input_parameter
+        data.at[10,'A'] = 'Constants'
+        data.at[10,'C'] = consts
+        data.at[10,'G'] = 'Entered Constants'
+        data.at[10,'I'] = user_input_consts
+        data.at[12,'G'] = 'Entered Data'
+        data.at[12,'I'] = user_input_path
+
+    elif format == 'CSV':
+        data = pd.DataFrame({
+            '1': ['Fitted Model:',  'Interpreted:',     'Raw:'],
+            '2': [fitted_model,     'Model:',           'Entered Model:'],
+            '3': [None,             model,              user_input_model],
+            '4': [None,             'Parameter:',       'Entered Parameter:'],
+            '5': [None,             parameter,          user_input_parameter],
+            '6': [None,             'Constants:',       'Entered Constants:'],
+            '7': [None,             consts,             user_input_consts],
+            '8': [None,             None,               'Entered Data:'],
+            '9': [None,             None,               user_input_path],
+        })
+
+    return data
+
+
+def get_valid_filename() -> str:
+    now = datetime.now()
+    return now.strftime("%Y-%m-%d-result-from-%Hh%Mm%Ss")
+
+
+def write_file(data_frame: pd.DataFrame, file_format: str = 'EXCEL') -> None:
+    relative_path = './res/'
+
+    if not os.path.exists(relative_path):
+        os.makedirs(relative_path)
+
+    file_name = get_valid_filename()
+
+    if file_format == 'EXCEL':
+        file_name = file_name + '.xlsx'
+        data_frame.to_excel(os.path.join(relative_path, file_name), index=False, header=False)
+    elif file_format == 'CSV':
+        file_name = file_name + '.csv'
+        data_frame.to_csv(os.path.join(relative_path, file_name), index=False, header=False, sep='\t')
+    else:
+        raise TypeError('Can\'t write to unknown file extension')
+
+
+def is_file_empty(file_path):
+    return os.stat(file_path).st_size == 0
+
+
+def main():
+    path = input("path: ")
+
+    # data = read_file(path)
+
+    # fdata = format_data(data, True)
+    # data = create_dataframe_from_for(format='CSV')
+    # write_file(data, file_format='CSV')
+
+    print(is_file_empty(path))
+
+
+if __name__ == '__main__':
+    main()
---
+++ deprecated/Unittests.py
@@ -0,0 +1,212 @@
+import unittest
+from deprecated.VPCData import VPCData
+from src.FileHandler import FileHandler
+
+class TestVPCData(unittest.TestCase):
+    """_summary_
+
+    _extended_summary_
+
+    :param unittest: _description_
+    :type unittest: _type_
+    """
+    def setUp(self):
+        print("Running setUp method...")
+        self.data_1 = VPCData(["a", "b", "c", "d"])
+        self.data_2 = VPCData([1.1, 2.2, 3.3, 4.4])
+        self.data_3 = VPCData(["a", 2.2, "c", 4.4])
+
+    def test_init(self):
+        print("Running __init__ tests...")
+        obj1 = VPCData()
+        obj2 = VPCData([])
+        obj3 = VPCData([1,2,3])
+        self.assertEqual(obj1.data, [])
+        self.assertEqual(obj2.data, [])
+        self.assertEqual(obj3.data, [1,2,3])
+        self.assertIsInstance(obj1, VPCData)
+        self.assertIsInstance(obj2, VPCData)
+        self.assertIsInstance(obj3, VPCData)
+
+    def test_getItem(self):
+        """_summary_
+
+        _extended_summary_
+        """
+        print("Running __getitem__ tests...")
+        self.assertEqual(self.data_1[0], "a")
+        self.assertEqual(self.data_2[1], 2.2)
+        self.assertEqual(self.data_3[2], "c")
+
+        self.assertEqual(self.data_1[:2], ["a", "b"])
+        self.assertEqual(self.data_2[1:3], [2.2, 3.3])
+        self.assertEqual(self.data_3[2:], ["c", 4.4])
+
+    def test_setItem(self):
+        print("Running __setItem__ tests...")
+        self.data_1[0] = 0.0
+        self.assertEqual(self.data_1, [0.0, "b", "c", "d"])
+        self.data_2[1] = "eins"
+        self.assertEqual(self.data_2, [1.1, "eins", 3.3, 4.4])
+        self.data_3[2] = 3
+        self.assertEqual(self.data_3, ["a", 2.2, 3, 4.4])
+
+    def test_len(self):
+        print("Running __len__ tests...")
+        obj4 = VPCData()
+        self.assertEqual(len(self.data_1), 4)
+        self.assertEqual(len(self.data_2), 4)
+        self.assertEqual(len(self.data_3), 4)
+        self.assertEqual(len(obj4), 0)
+
+    def test_delitem(self):
+        print("Running __delitem__ tests...")
+        del self.data_1[1]
+        self.assertEqual(self.data_1, ["a", "c", "d"])
+        del self.data_1[0]
+        self.assertEqual(self.data_1, ["c", "d"])
+        del self.data_1[1]
+        self.assertEqual(self.data_1, ["c"])
+        del self.data_1[0]
+        self.assertEqual(self.data_1, [])
+
+    def test_append(self):
+        print("Running append tests...")
+        self.data_1.append("e")
+        self.assertEqual(self.data_1, ["a", "b", "c", "d", "e"])
+        self.data_2.append([5.5, 6.6])
+        self.assertEqual(self.data_2, [1.1, 2.2, 3.3, 4.4, 5.5, 6.6])
+        self.data_3.append([])
+        self.assertEqual(self.data_3, ["a", 2.2, "c", 4.4])
+
+    def test_insert(self):
+        print("Running insert tests...")
+        self.data_1.insert(0, "z")
+        self.assertEqual(self.data_1, ["z", "a", "b", "c", "d"])
+        self.data_2.insert(2, [2.3, 2.4, 2.5])
+        self.assertEqual(self.data_2, [1.1, 2.2, 2.3, 2.4, 2.5, 3.3, 4.4])
+        self.data_2.insert(-1, 3.6)
+        self.assertEqual(self.data_2, [1.1, 2.2, 2.3, 2.4, 2.5, 3.3, 3.6, 4.4])
+        self.data_3.insert(0, [])
+        self.assertEqual(self.data_3, ["a", 2.2, "c", 4.4])
+
+    def test_remove(self):
+        print("Running remove tests...")
+        self.data_1.remove("a")
+        self.assertEqual(self.data_1, ["b", "c", "d"])
+        with self.assertRaises(ValueError):
+            self.data_2.remove(5.5)
+
+    def test_index(self):
+        print("Running index tests...")
+        self.assertEqual(self.data_2.index(1.1), 0)
+        with self.assertRaises(ValueError):
+            self.data_1.index(5)
+        with self.assertRaises(ValueError):
+            self.data_2.index([2.2, 3.3])
+
+class TestFileHandler(unittest.TestCase):
+    """_summary_
+
+    _extended_summary_
+
+    :param unittest: _description_
+    :type unittest: _type_
+    """
+
+    def setUp(self):
+        print("Running setUp method...")
+        self.handler_r = FileHandler.ReadMode(".")
+        self.handler_w = FileHandler.WriteMode(".", "filey", "xml", [1,2,3])
+
+    def test_init(self):
+        """_summary_
+
+        _extended_summary_
+        """
+        print("Running __init__ tests...")
+        with self.assertRaises(Exception):
+            handler_blank0 = FileHandler()
+        with self.assertRaises(Exception):
+            handler_blank1 = FileHandler('.')
+
+    def test_getsetMode(self):
+        print("Running setMode and getMode tests...")
+        r = 'READ'
+        w = 'WRITE'
+        self.assertEqual(self.handler_r.mode, r)
+        self.assertEqual(self.handler_w.mode, w)
+        with self.assertRaises(Exception):
+            self.handler_r.mode = None
+        self.handler_r.mode = 'rEaD'
+        self.assertEqual(self.handler_r.mode, 'READ')
+        self.handler_w.mode = 'write'
+        self.assertEqual(self.handler_w.mode, 'WRITE')
+        with self.assertRaises(Exception):
+             self.handler_r.mode = 1
+        with self.assertRaises(Exception):
+             self.handler_r.mode = 'ead'
+        with self.assertRaises(Exception):
+             self.handler_r.mode = 'rite'
+
+    def test_getsetPath(self):
+        print("Running getPath and setPath tests...")
+        with self.assertRaises(ValueError):
+            self.handler_r.path = "./<filename>"
+        self.handler_w.path = "./classes"
+        self.assertEqual(self.handler_w.path, "./classes")
+        with self.assertRaises(ValueError):
+            self.handler_w.path = None
+        with self.assertRaises(ValueError):
+            self.handler_r.path = 123
+
+    def test_getsetFilename(self):
+        print("Running getFilename and setFilename tests...")
+        with self.assertRaises(ValueError):
+            self.handler_r.fileName = 123
+        with self.assertRaises(ValueError):
+            self.handler_w.fileName = "file.name"
+        self.handler_w.fileName = "file1"
+        self.assertEqual(self.handler_w.fileName, "file1")
+        with self.assertRaises(ValueError):
+            self.handler_r.fileName = None
+
+    def test_getsetFileFormat(self):
+        print("Running getFileFormat and setFileFormat tests...")
+        with self.assertRaises(ValueError):
+            self.handler_r.fileFormat = "PDF"
+        self.handler_w.fileFormat = 'XML'
+        self.assertEqual(self.handler_w.fileFormat, 'XML')
+        with self.assertRaises(ValueError):
+            self.handler_r.fileFormat = 123
+        with self.assertRaises(ValueError):
+            self.handler_r.fileFormat = None
+
+    # def test_open(self):
+    #     pass
+
+    # def test_readFile(self):
+    #     pass
+
+    # def test_writeFile(self):
+    #     pass
+
+
+# def run_tests():
+#     test_classes_to_run = [TestVPCData, TestFileHandler]
+
+#     loader = unittest.TestLoader()
+
+#     suites_list = []
+#     for test_class in test_classes_to_run:
+#         suite = loader.loadTestsFromTestCase(test_class)
+#         suites_list.append(suite)
+
+#     big_suite = unittest.TestSuite(suites_list)
+
+#     runner = unittest.TextTestRunner()
+#     results = runner.run(big_suite)
+
+if __name__ == "__main__":
+    # run_tests()
+    unittest.main()
